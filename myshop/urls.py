b459a5a5d12eacde6ad44e73027a356e7f9edb8a
Gaurav Verma <mcjail.shi.hp@gmail.com>
2016-10-17 07:16:41 +0530

    added redirect

--- myshop/urls.py
+++ myshop/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    url(r'^$',RedirectView.as_view(pattern_name='shop:product_list', permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^shop/', include('shop.urls', namespace='shop')),
     url(r'^cart/', include('cart.urls', namespace='cart')),
+
 ]
 
 if settings.DEBUG:
