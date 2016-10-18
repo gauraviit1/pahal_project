43982ce9d1518ad4a902bec33b3c25c5f46a94d1
Gaurav Verma <mcjail.shi.hp@gmail.com>
2016-10-18 09:17:10 +0530

    Added orders model and form

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from orders.models import OrderItem
+from orders.forms import OrderCreateForm
+from cart.cart import Cart
 
 # Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST'
