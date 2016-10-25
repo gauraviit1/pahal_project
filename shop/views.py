3d129cad72b417894eaffce846df6c4df15181a9
Gaurav Verma <mcjail.shi.hp@gmail.com>
2016-10-25 09:13:39 +0530

    added testimonial in template

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from shop.models import Cateogry, Product
 from cart.forms import CartAddProductForm
-
+from testimonials.models import Testimonial_
 # Create your views here.
 def product_list(request, cateogry_slug=None):
     cateogry = None
     cateogries = Cateogry.objects.all()
+    testimonials = Testimonial_.objects.all()[:5]
     products = Product.objects.filter(available=True)
     if cateogry_slug:
         cateogry = get_object_or_404(Cateogry,slug=cateogry_slug)
@@ -14,6 +15,7 @@ def product_list(request, cateogry_slug=None):
         'cateogry': cateogry,
         'cateogries': cateogries,
         'products': products,
+        'testimonials':testimonials,
     })
 
 def product_detail(request, id, slug):
