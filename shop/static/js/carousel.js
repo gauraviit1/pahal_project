14a27cbdcf74a7ae0e1b991ce237719fce872620
gauraviit1 <mcjail.shi.hp@gmail.com>
2016-12-25 08:34:01 +0530

    changed main carousel id

--- shop/static/js/carousel.js
+++ shop/static/js/carousel.js
@@ -1,7 +1,31 @@
-$(document).ready(function () {
-    $('.carousel').carousel({
-        interval: 500
-    });
 
-    $('.carousel').carousel('cycle');
-});
+
+// dfdvfdv
+
+(function(){
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // http://getbootstrap.com/javascript/#carousel
+  $('#carousel123').carousel({ interval: 2000 });
+
+}());
+
+(function(){
+  $('.carousel-showsixmoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<6;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
